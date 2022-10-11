aa56f156e6efdafd7be0f79a378c34c78362fabd
Nickolas Eduardo <nickolasvieira.el@gmail.com>
2022-10-11 15:54:44 -0300

    checksExistsUserAccount + checksCreateTodosUserAvail. done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 
 const { v4: uuidv4, validate } = require('uuid');
 
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -10,19 +11,60 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } =  request.headers
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({ error: "404 - Username not found"})
+  }
+
+  request.user = user
+  return next()
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+  const tasks = user.todos
+
+  if(!user.pro && tasks.length > 9) {
+    return response.status(403).json({ error: "Not allowed, only Pro accounts proceed from here" })
+  }
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request
+  const { id } = request.params
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  if(verifyId === id) {
+    return response.status(400).json({ error: "This not a valid ID" })
+  }
+
+  request.user = user
+  request.todo = id
+  return next()
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(user => user.id === id)
+
+  if(!user) {
+    return response.status(404)
+  }
+
+  request.user = user
+  return next()
+
 }
 
 app.post('/users', (request, response) => {
